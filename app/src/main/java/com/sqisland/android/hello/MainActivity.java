fe9bc6b436c6ac1b3a74789528890b1f72a33571
liuyufei <gabriel.liu3615@gmail.com>
2017-03-28 21:06:12 +1300

    thread

--- app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -2,6 +2,8 @@ package com.sqisland.android.hello;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,6 +12,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.sqisland.android.hello.ui.AnimationActivity;
 import com.sqisland.android.hello.ui.FragementShow;
 import com.sqisland.android.hello.ui.FragmentTest;
 import com.sqisland.android.hello.ui.GestureActivity;
@@ -21,6 +24,13 @@ public class MainActivity extends FragmentActivity implements FragmentTest.OnFra
     Button button1;
     Button button2;
 
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            description.setText(" after 5 seconds, Nice Job Thread !!!");
+        }
+    };
 
 
     @Override
@@ -66,18 +76,41 @@ public class MainActivity extends FragmentActivity implements FragmentTest.OnFra
     public boolean onOptionsItemSelected(MenuItem item) {
         //response to the menu item click
         switch (item.getItemId()) {
-            case R.id.settings:
-                Toast.makeText(this,"click settings menu item", Toast.LENGTH_SHORT).show();
+            case R.id.send_broadcast:
+                //create a broadcast intent
+                Intent i = new Intent();
+                i.setAction("com.sqisland.android.hello");
+                i.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+                sendBroadcast(i);
                 break;
-            case R.id.profile:
-                Toast.makeText(this,"click profile menu item", Toast.LENGTH_SHORT).show();
+            case R.id.animation:
+                Toast.makeText(this,"click animation menu item", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, AnimationActivity.class);
+                startActivity(intent);
                 break;
-            case R.id.logout:
-                Toast.makeText(this,"click logout menu item", Toast.LENGTH_SHORT).show();
+            case R.id.thread:
+                Toast.makeText(this,"click thread menu item", Toast.LENGTH_SHORT).show();
+                startThread();
                 break;
             default: break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void startThread() {
+        //create and start a thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                handler.sendEmptyMessage(0);
+            }
+        }).start();
+
     }
 
 
---
+++ app/src/main/java/com/sqisland/android/hello/ui/animationActivity.java
@@ -0,0 +1,60 @@
+package com.sqisland.android.hello.ui;
+
+import android.app.Activity;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.sqisland.android.hello.R;
+
+public class AnimationActivity extends Activity {
+
+    ViewGroup sbLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation);
+
+        sbLayout = (ViewGroup) findViewById(R.id.activity_animation);
+
+        sbLayout.setOnTouchListener(new RelativeLayout.OnTouchListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                moveButton();
+                return true;
+            }
+        });
+
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void moveButton(){
+        View moveButton = findViewById(R.id.moveButton);
+
+        TransitionManager.beginDelayedTransition(sbLayout);
+
+        //change the position of the button
+        RelativeLayout.LayoutParams posistionRules = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        posistionRules.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,RelativeLayout.TRUE);
+        posistionRules.addRule(RelativeLayout.ALIGN_PARENT_RIGHT,RelativeLayout.TRUE);
+        moveButton.setLayoutParams(posistionRules);
+
+        //Change the size of the button
+        ViewGroup.LayoutParams sizeRule = moveButton.getLayoutParams();
+        sizeRule.width = 450;
+        sizeRule.height = 300;
+        moveButton.setLayoutParams(sizeRule);
+
+    }
+}
